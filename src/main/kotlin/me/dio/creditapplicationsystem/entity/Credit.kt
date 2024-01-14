a1ab6f241c95b3f60be9d7b3c5cea3bb8d44d010
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-01-14 16:41:04 -0300

    addinterfaces to create logic persistence layer connect BD

--- src/main/kotlin/me/dio/creditapplicationsystem/entity/Credit.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/entity/Credit.kt
@@ -9,10 +9,13 @@ import java.util.UUID
 @Entity
 @Table(name = "Credit")
 data class Credits (
-    @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false, unique = true)
+    val creditCode: UUID = UUID.randomUUID(),
     val creditValue: BigDecimal = BigDecimal.ZERO,
-    @Column(nullable = false) val dayFirstInstallment: LocalDate,
-    @Column(nullable = false) val numberOfInstallment: Int = 0,
+    @Column(nullable = false)
+    val dayFirstInstallment: LocalDate,
+    @Column(nullable = false)
+    val numberOfInstallment: Int = 0,
     @Enumerated val status: Status = Status.INPROGRESS,
     //Muitos credits podem pertencer a um customer
     @ManyToOne val customer: Customer? = null,
