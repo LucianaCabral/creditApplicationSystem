8b978f870e3d1cf1009b845338762470e492f872
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-01-15 12:18:20 -0300

    implement method findById to get data credit to creditCode

--- src/main/kotlin/me/dio/creditapplicationsystem/data/controller/CreditController.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/data/controller/CreditController.kt
@@ -1,10 +1,12 @@
 package me.dio.creditapplicationsystem.data.controller
 
 import me.dio.creditapplicationsystem.data.controller.dto.CreditDto
+import me.dio.creditapplicationsystem.data.controller.dto.CreditView
 import me.dio.creditapplicationsystem.data.controller.dto.CreditViewList
 import me.dio.creditapplicationsystem.data.dao.entity.Credit
 import me.dio.creditapplicationsystem.data.service.CreditService
 import org.springframework.web.bind.annotation.*
+import java.util.*
 import java.util.stream.Collectors
 
 @RestController
@@ -23,5 +25,14 @@ class CreditController(
     fun findByAllCustomerId(@RequestParam(value = "customerId") customerId: Long): List<CreditViewList> {
         return this.creditService.findAllByCustomer(customerId = customerId).stream()
             .map { credit: Credit -> CreditViewList(credit) }.collect(Collectors.toList())
+    }
+
+    @GetMapping
+    fun findByCreditCode(
+        @RequestParam(value = "customerId") customerId: Long,
+        @PathVariable creditCode: UUID
+    ): CreditView {
+        val credit: Credit = this.creditService.findByCreditCode(customerId, creditCode)
+        return CreditView(credit)
     }
 }
