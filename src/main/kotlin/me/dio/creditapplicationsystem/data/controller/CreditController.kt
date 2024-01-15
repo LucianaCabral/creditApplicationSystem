cc17b4f30ffb4a6e2a8d3c3076a0e31292306bbb
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-01-15 11:15:52 -0300

    implement endpoint and methods findByAllCustomer

--- src/main/kotlin/me/dio/creditapplicationsystem/data/controller/CreditController.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/data/controller/CreditController.kt
@@ -1,11 +1,14 @@
 package me.dio.creditapplicationsystem.data.controller
 
 import me.dio.creditapplicationsystem.data.controller.dto.CreditDto
+import me.dio.creditapplicationsystem.data.controller.dto.CreditViewList
 import me.dio.creditapplicationsystem.data.dao.entity.Credit
 import me.dio.creditapplicationsystem.data.service.CreditService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.stream.Collectors
 
 @RestController
 @RequestMapping("/api/credits")
@@ -14,8 +17,14 @@ class CreditController(
 
 ) {
     @PostMapping
-    fun saveCredit(creditDto: CreditDto) : String {
-    val credit: Credit = this.creditService.save(credit = creditDto.toEntity())
+    fun saveCredit(creditDto: CreditDto): String {
+        val credit: Credit = this.creditService.save(credit = creditDto.toEntity())
         return "credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved"
+    }
+
+    @GetMapping
+    fun findByAllCustomerId(customerId: Long): List<CreditViewList> {
+        return this.creditService.findAllByCustomer(customerId = customerId).stream()
+            .map { credit: Credit -> CreditViewList(credit) }.collect(Collectors.toList())
     }
 }
--- src/main/kotlin/me/dio/creditapplicationsystem/data/controller/dto/CreditViewList.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/data/controller/dto/CreditViewList.kt
@@ -4,14 +4,16 @@ import me.dio.creditapplicationsystem.data.dao.entity.Credit
 import java.math.BigDecimal
 import java.time.LocalDate
 
-data class CreditDto(
+data class CreditViewList(
     val creditValue: BigDecimal,
     val dayFirstOfInstallment: LocalDate,
     val numberOfInstallment: Int,
+    val customId: Long
 ) {
-    fun toEntity(): Credit = Credit(
-        creditValue = this.creditValue,
-        dayFirstInstallment = this.dayFirstOfInstallment,
-        numberOfInstallment = this.numberOfInstallment,
+    constructor(credit: Credit) : this(
+        creditValue = credit.creditValue,
+        dayFirstOfInstallment = credit.dayFirstInstallment,
+        numberOfInstallment = credit.numberOfInstallment,
+        customId = credit.id ?: 0
     )
 }
