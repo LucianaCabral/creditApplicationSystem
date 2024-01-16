5ff034f0a8f0b91a1234d88ae9e39122ca26b25f
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-01-15 21:12:29 -0300

    fix CreditViewist

--- src/main/kotlin/me/dio/creditapplicationsystem/data/dto/response/CreditViewList.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/data/dto/response/CreditViewList.kt
@@ -3,14 +3,15 @@ package me.dio.creditapplicationsystem.data.dto.response
 import me.dio.creditapplicationsystem.data.dao.entity.Credit
 import java.math.BigDecimal
 import java.time.LocalDate
+import java.util.UUID
 
 data class CreditViewList(
-    val creditValue: BigDecimal,
+    val creditCode: UUID,
     val dayFirstOfInstallment: LocalDate,
     val numberOfInstallment: Int,
 ) {
     constructor(credit: Credit) : this(
-        creditValue = credit.creditValue,
+        creditCode = credit.creditCode,
         dayFirstOfInstallment = credit.dayFirstInstallment,
         numberOfInstallment = credit.numberOfInstallment,
     )
