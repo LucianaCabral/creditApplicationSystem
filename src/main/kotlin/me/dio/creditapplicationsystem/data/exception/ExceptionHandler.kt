14770e1a08df3f11a6ac0cf19842f75fd0d4e420
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-01-15 22:57:32 -0300

    create handle exception MethodArgumentNotValidException

---
+++ src/main/kotlin/me/dio/creditapplicationsystem/data/exception/ExceptionHandler.kt
@@ -0,0 +1,35 @@
+package me.dio.creditapplicationsystem.data.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.FieldError
+import org.springframework.validation.ObjectError
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+@RestControllerAdvice
+class ExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handlerValidException(ex: MethodArgumentNotValidException): ResponseEntity<ExceptionsDetails> {
+        val erros: MutableMap<String, String?> = HashMap()
+        ex.bindingResult.allErrors.stream().forEach { erro: ObjectError ->
+            val fieldName: String = (erro as FieldError).field
+            val messageError: String? = erro.defaultMessage
+            erros[fieldName] = messageError
+        }
+        return ResponseEntity(
+            ExceptionsDetails(
+                title = "Bad Request! Consult the Documentation",
+                timeStamp = LocalDateTime.now(),
+                status = HttpStatus.BAD_REQUEST.value(),
+                exception = ex.javaClass.simpleName,
+                details = erros
+            ),
+            HttpStatus.BAD_REQUEST
+        )
+    }
+}
