e286b933f35127233937be3b67af01e6a268f280
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-02-15 19:21:34 -0300

    test: Create unit test save credit

--- src/test/kotlin/me/dio/creditapplicationsystem/CustomerServiceTest.kt
+++ src/test/kotlin/me/dio/creditapplicationsystem/CustomerServiceTest.kt
@@ -3,13 +3,12 @@ package me.dio.creditapplicationsystem
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import me.dio.creditapplicationsystem.data.dao.entity.Address
+import me.dio.creditapplicationsystem.StubCustomer.buildCustomer
 import me.dio.creditapplicationsystem.data.dao.entity.Customer
 import me.dio.creditapplicationsystem.data.service.CustomerServiceImpl
 import me.dio.creditapplicationsystem.data.service.repository.CustomerRepository
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
-import java.math.BigDecimal
 
 class CustomerServiceTest {
     private val repository : CustomerRepository = mockk()
@@ -19,6 +18,7 @@ class CustomerServiceTest {
     fun `should create customer`() {
         //given
         val fakeCustomer: Customer = buildCustomer()
+
         every { repository.save(fakeCustomer) } returns fakeCustomer
 
         //when
@@ -29,28 +29,4 @@ class CustomerServiceTest {
         Assertions.assertThat(actual).isSameAs(fakeCustomer)
         verify(exactly = 1) { repository.save(fakeCustomer) }
     }
-
-    private fun buildCustomer(
-        firstName: String = "Lorem",
-        lastName: String = "Ipsum",
-        cpf: String = "28475934625",
-        email: String = "loremIpsum@gmail.com",
-        password: String = "12345",
-        zipCode: String = "12345",
-        street: String = "Rua das Palmeiras",
-        income: BigDecimal = BigDecimal.valueOf(1000.0),
-        id: Long = 1L
-    ) = Customer(
-        firstName = firstName,
-        lastName = lastName,
-        cpf = cpf,
-        email = email,
-        password = password,
-        address = Address(
-            zipCode = zipCode,
-            street = street,
-        ),
-        income = income,
-        id = id
-    )
 }
--- src/test/kotlin/me/dio/creditapplicationsystem/StubCustomer.kt
+++ src/test/kotlin/me/dio/creditapplicationsystem/StubCustomer.kt
@@ -6,41 +6,30 @@ import me.dio.creditapplicationsystem.data.dao.entity.Customer
 import java.math.BigDecimal
 import java.time.LocalDate
 
-object StubCustomer {
+internal object StubCustomer {
 
-//     fun buildCustomer(
-//        firstName: String = "Cami",
-//        lastName: String = "Cavalcante",
-//        cpf: String = "28475934625",
-//        email: String = "camila@gmail.com",
-//        password: String = "12345",
-//        zipCode: String = "12345",
-//        street: String = "Rua da Cami",
-//        income: BigDecimal = BigDecimal.valueOf(1000.0),
-//        id: Long = 1L
-//    ) = Customer(
-//        firstName = firstName,
-//        lastName = lastName,
-//        cpf = cpf,
-//        email = email,
-//        password = password,
-//        address = Address(
-//            zipCode = zipCode,
-//            street = street,
-//        ),
-//        income = income,
-//        id = id
-//    )
+     fun buildCustomer(
+        firstName: String = "Lorem",
+        lastName: String = "Ipsum",
+        cpf: String = "28475934625",
+        email: String = "loremIpsum@gmail.com",
+        password: String = "12345",
+        zipCode: String = "12345",
+        street: String = "Rua das Palmeiras",
+        income: BigDecimal = BigDecimal.valueOf(1000.0),
+        id: Long = 1L
+    ) = Customer(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        password = password,
+        address = Address(
+            zipCode = zipCode,
+            street = street,
+        ),
+        income = income,
+        id = id
+    )
 
-//    fun buildCredit(
-//        creditValue: BigDecimal = BigDecimal.valueOf(100.0),
-//        dayFirstInstallment: LocalDate = LocalDate.now().plusMonths(2L),
-//        numberOfInstallments: Int = 15,
-//        customer: Customer = buildCustomer()
-//    ): Credit = Credit(
-//        creditValue = creditValue,
-//        dayFirstInstallment = dayFirstInstallment,
-//        numberOfInstallment = numberOfInstallments,
-//        customer = customer
-//    )
 }
