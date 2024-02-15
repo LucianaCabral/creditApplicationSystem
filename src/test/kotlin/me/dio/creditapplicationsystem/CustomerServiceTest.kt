2069e2087b2aaa372bfc2235f8d43259a21df9fe
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-02-15 20:43:38 -0300

    test: Create unit test findById credit

--- src/test/kotlin/me/dio/creditapplicationsystem/CustomerServiceTest.kt
+++ src/test/kotlin/me/dio/creditapplicationsystem/CustomerServiceTest.kt
@@ -9,9 +9,11 @@ import me.dio.creditapplicationsystem.data.service.CustomerServiceImpl
 import me.dio.creditapplicationsystem.data.service.repository.CustomerRepository
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import java.util.Optional
+import java.util.Random
 
 class CustomerServiceTest {
-    private val repository : CustomerRepository = mockk()
+    private val repository: CustomerRepository = mockk()
     private val customerService = CustomerServiceImpl(repository)
 
     @Test
@@ -28,5 +30,23 @@ class CustomerServiceTest {
         Assertions.assertThat(actual).isNotNull
         Assertions.assertThat(actual).isSameAs(fakeCustomer)
         verify(exactly = 1) { repository.save(fakeCustomer) }
+    }
+
+    @Test
+    fun `should create findById by id`() {
+        //given
+        val fakeId: Long = Random().nextLong()
+        val fakeCustomer: Customer = buildCustomer(id = fakeId)
+
+        every { repository.findById(fakeId)} returns Optional.of(fakeCustomer)
+
+        //when
+        val actual: Customer = customerService.findById(fakeId)
+
+        //then
+        Assertions.assertThat(actual).isNotNull
+        Assertions.assertThat(actual).isExactlyInstanceOf(Customer::class.java)
+        Assertions.assertThat(actual).isSameAs(fakeCustomer)
+        verify(exactly = 1) { repository.findById(fakeId) }
     }
 }
